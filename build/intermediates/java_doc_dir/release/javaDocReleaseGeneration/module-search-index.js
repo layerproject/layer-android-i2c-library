533999846712fb2fbbb402b70ee56d53b74710ea
Gilles Dubuc <gilles@layer.com>
2025-04-22 11:24:00 +0200

    Remove build files from git tracking

--- build/intermediates/java_doc_dir/release/javaDocReleaseGeneration/module-search-index.js
+++
@@ -1,1 +0,0 @@
-var moduleSearchIndex = [{"l":":","url":"index.html"}]
--- build/intermediates/java_doc_dir/release/javaDocReleaseGeneration/package-search-index.js
+++
@@ -1,1 +0,0 @@
-var packageSearchIndex = [{"l":"com.layer.i2c","url":"com/layer/i2c/package-summary.html"}, {"l":"All packages","url":"index.html"}]
